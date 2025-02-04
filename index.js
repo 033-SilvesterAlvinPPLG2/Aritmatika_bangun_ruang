5697f731fdddbb5c48d1581372202a640165ae03
Akv1gn <oktavianoalvin1@gmail.com>
2025-02-04 14:15:36 +0700

    Aritmatika Bangun Ruang

---
+++ index.js
@@ -0,0 +1,46 @@
+{
+    let a = 10; //panjang 
+    let b = 10; //lebar           //mencari luas persegi 
+    let hasil = a * b
+    console.log (hasil)
+}
+
+{
+    let a = 10; //panjang
+    let b = 10; //lebar           //mencari keliling persegi
+    let hasil = a + b + a + b
+    console.log (hasil)
+}
+
+{
+    let a = 15; //panjang
+    let b = 6; //lebar             //mencari luas persegi panjang
+    let hasil = a * b 
+    console.log(hasil)
+}
+
+{
+    let a = 15; //panjang
+    let b = 6; //lebar            //mencari keliling persegi panjang
+    let hasil = 2 * (a + b)
+    console.log(hasil)
+}
+
+{
+    let a = 30; //tiap sisi
+    let b = 15; //tinggi           //mencari luas segitiga
+    let hasil = 1/2 * a * b
+    console.log(hasil)
+}
+
+{
+    let a = 30; //tiap sisi
+    let b = 15; //tinggi            //mencari keliling segitiga
+    let hasil = a + a + a
+    console.log(hasil)
+}
+
+{
+let nama = "owen"
+console.log ( "nama saya " + (nama))
+}
